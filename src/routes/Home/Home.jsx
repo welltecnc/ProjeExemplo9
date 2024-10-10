7f180f873f49136f6e7e8426d5776a8accf048c0
Wellington <welltecnic@hotmail.com>
2024-10-10 09:55:13 -0300

    ajustes

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react"
+
 const Home =()=>{
+
+    const [produto, setProduto]=useState([])
+
+    useEffect(()=>{
+        fetch("http://localhost:5000/produto")
+        .then((res)=>{
+            return res.json()
+        })
+        .then((data)=>{
+            setProduto(data)
+        })
+    },[])
+
     return(
         <>
-        <h1>Home</h1>
+        <div>
+            {produto.map((item, index)=>(
+                <div key={index}>
+                    {item.nome}
+                   <img src={item.foto}/>
+                </div>
+            ))}            
+        </div>
         </>
     )
 }
